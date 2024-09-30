9c80cf146c8735c6b841e39b234d38e2fbcbbee7
Uriel <leirualexisparantar@gmail.com>
2024-09-30 16:48:59 +0800

    Add project files.

---
+++ AliacSearchAlgo/Fringe2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AliacSearchAlgo;
+
+namespace AISearchSample {
+    class Fringe2 : Fringes {
+        Queue<Node> queue;
+
+        public Fringe2() {
+            queue = new Queue<Node>();
+        }
+        
+        public void add(Node n, Node origin) {
+            n.Origin = origin;
+            queue.Enqueue(n);  
+        }
+
+        // Rem
+        public Node remove() {
+            if (queue.Count != 0)
+                return queue.Dequeue();  // Dequ
+            return null;
+        }
+    }
+}
